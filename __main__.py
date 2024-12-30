2fce2432852c7324e3fedeca993c40aa98f66410
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2024-12-29 18:11:53 -0800

    updated file opening + created tracklist

--- __main__.py
+++ __main__.py
@@ -1,7 +1,12 @@
 import csv
 from track import Track
 
+track_list = []
 
+with open('circuits.csv', 'r') as circuits:
+    csv_reader = csv.reader(circuits)
+    next(csv_reader, None)
+    for circuitId, circuitRef, name, location, country, lat, lng, alt, url in csv_reader:
+        track_list.append(Track(circuitId, circuitRef, name, location, country, lat, lng, alt, url))
 
-
-
+print(track_list)
