4405de2006b55a248b898350afa3e16f8670896c
hkandaswamy <77952068+hkandaswamy@users.noreply.github.com>
2024-12-29 22:44:02 -0800

    added encapsulation

--- track.py
+++ track.py
@@ -1,15 +1,51 @@
 class Track:
 
     def __init__(self, id, ref, name, location, country, lat, lng, alt, url):
-        self.id = id
-        self.ref = ref
-        self.name = name
-        self.location = location
-        self.country = country
-        self.lat = lat
-        self.lng = lng
-        self.alt = alt
-        self.url = url
+        self.__id = id
+        self.__ref = ref
+        self.__name = name
+        self.__location = location
+        self.__country = country
+        self.__lat = lat
+        self.__lng = lng
+        self.__alt = alt
+        self.__url = url
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def ref(self):
+        return self.__ref
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def location(self):
+        return self.__location
+    
+    @property
+    def country(self):
+        return self.__country
+    
+    @property
+    def lat(self):
+        return self.__lat
+    
+    @property
+    def lng(self):
+        return self.__lng
+    
+    @property
+    def alt(self):
+        return self.__alt
+    
+    @property
+    def url(self):
+        return self.__url
     
     def to_dict(self):
         return {
